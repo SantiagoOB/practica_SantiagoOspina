03374255ff0e506e28fdb70dcec725e43776ff9d
SantiagoOB <santiagospina1608@gmail.com>
2021-12-19 16:54:55 +0100

    Update 2

--- practica_SantiagoOspina/src/practica_SantiagoOspina/practica_SantiagoOspina.java
+++ practica_SantiagoOspina/src/practica_SantiagoOspina/practica_SantiagoOspina.java
@@ -6,17 +6,14 @@ public class practica_SantiagoOspina {
 	public static void main(String[] args) {
 		printOne();
 		printOne();
-		printTwo();
+		
 	}
 
 	public static void printOne() {
 		System.out.println("Hello World");
 	}
 
-	public static void printTwo() {
-		printOne();
-		printOne();
-	}
+	
 
 	
 
